08413542d51786a4b6038fc71b782ad41b4f09c9
sone <sthaya@gmail.com>
2021-11-08 17:19:08 -0800

    setup photo upload and frontend react

--- DjangoAPI/EmployeeApp/urls.py
+++ DjangoAPI/EmployeeApp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from EmployeeApp import views
 
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns=[
@@ -9,4 +11,6 @@ urlpatterns=[
 
     url(r'employee$', views.employeeApi),
     url(r'employee/([0-9]+)$', views.employeeApi),
-]
+
+    url(r'^employee/savefile',views.SaveFile)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
